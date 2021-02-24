e5bacc0fc357fdba750148c4a47422f71eedd2e5
Jose <jose42cordova@gmail.com>
2021-02-24 10:54:11 -0600

    can update database through HTML

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -8,15 +8,30 @@ module.exports = (app) => {
     });
 
     app.post('/api/posts', (req, res) => {
-        console.log(req.body, '...api-routes.js:5');
-        console.log(req.body.state, '...api-routes.js:6');
-        console.log(req.body.deed, '...api-routes.js:7');
+        console.log(req.body, '...api-routes.js:11');
+        console.log(req.body.state, '...api-routes.js:12');
+        console.log(req.body.deed, '...api-routes.js:13');
         db.Posts.create({
             state: req.body.state,
             deed: req.body.deed,
             completed: false,
         }).then((dbPost) => res.json(dbPost));
 
+    });
+
+    app.put('/api/posts', (req, res) => {
+        console.log(req.body.completed, '...api-routes.js:23');
+        console.log(req.body.id, '...api-routes.js:24');
+        db.Posts.update(
+            {
+                completed: req.body.completed,
+            },
+            {
+                where: {
+                    id: req.body.id,
+                },
+            }
+        ).then((dbPost) => res.json(dbPost));
     });
 
 };
