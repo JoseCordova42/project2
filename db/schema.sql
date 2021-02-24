21b6a83d50aafbd4d75bd5fb6117d911bfb58892
Dan Bushong <bushongdan@gmail.com>
2021-02-24 11:59:05 -0600

    schema, connection, env and graph

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,14 @@
-DROP DATABASE IF EXISTS deeds_db;
+use eid7kgl3y6npef5j;
 
-CREATE DATABASE deeds_db;
+CREATE TABLE Posts(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  state VARCHAR(255),
+  deed text,
+  completed TINYINT,
+  createdAt datetime,
+  updatedAt datetime
+
+);
 
 
 
